5888068c7e3c644683896c9f04bddc98d4c2919c
qq137321 <129837293+qq137321@users.noreply.github.com>
2023-08-11 05:04:08 +0800

    Update polly.js

--- src/services/polly.js
+++ src/services/polly.js
@@ -1,7 +1,7 @@
 import { PollyClient } from "@aws-sdk/client-polly";
 
 const client = new PollyClient({
-  region: "eu-west-3",
+  region: "us-east-1",
   credentials: {
     accessKeyId: process.env.REACT_APP_AMAZON_ACCESS_KEY_ID,
     secretAccessKey: process.env.REACT_APP_AMAZON_SECRET_ACCESS_KEY,
